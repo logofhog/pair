d1b86b7470ea7b225e4be81c0e60482a2be2f391
logofhog <thomasscoggins@gmail.com>
2014-08-05 00:21:28 -0400

    added cache for zip code validator

--- lib/zip_code_lookup.rb
+++ lib/zip_code_lookup.rb
@@ -2,11 +2,24 @@ require 'net/http'
 
 class ZipCodeLookup
 
-  URL = 'http://www.webservicex.net/uszip.asmx/GetInfoByZIP?USZip='
-
   def self.valid?(zip_code)
+    ZipCodeCache.get_or_cache(zip_code)
+  end
+
+end
+
+class ZipCodeCache
+  
+  URL = 'http://www.webservicex.net/uszip.asmx/GetInfoByZIP?USZip='
+  @cached_zip_codes= {}
+
+  def self.get_or_cache(zip_code)
+    @cached_zip_codes[zip_code] || call_api(zip_code)
+  end
+
+  def self.call_api(zip_code)
     response = Net::HTTP.get(URI.parse(URL + zip_code.to_s))
-    parse_response(response)
+    @cached_zip_codes[zip_code] = parse_response(response)
   end
 
   def self.parse_response(response)

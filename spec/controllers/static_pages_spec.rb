cbdab257df0056528590c05ef6c2a1c0336f9d46
thomas <thomasscoggins@gmail.com>
2014-08-16 10:18:15 -0400

    fixed test

--- spec/controllers/static_pages_spec.rb
+++ spec/controllers/static_pages_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
+  before do
+    @user = FactoryGirl.create :user
+    sign_in @user
+  end
+
   it 'should work' do
     get 'index'
     expect(response).to be_success
   end
 
   it 'current user is correct' do
-    @user = FactoryGirl.create :user
-    sign_in @user
     get 'index'
     expect(assigns(:current_user)).to eq(@user)
   end
+
 
 end
